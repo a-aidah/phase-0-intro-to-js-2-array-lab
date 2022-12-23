30993e7df1c4bacb65c0f18ec6631c7f72665008
Abdullah Omer Aidah <a.o.aidah.note9@gmail.com>
2022-12-24 01:54:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const coolCats = [...cats, name];
+    return coolCats;
+}
+
+function prependCat(name) {
+    const coolCats = [name, ...cats];
+    return coolCats;
+}
+
+function removeLastCat() {
+    const Scats = cats.slice(0, cats.length - 1);
+    return Scats;
+}
+
+function removeFirstCat() {
+    const coolCats = cats.slice(1)
+    return coolCats;
+}
